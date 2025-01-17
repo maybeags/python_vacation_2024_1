1bbee4bd5a26bc536327d7f7ce52799561f1feb3
maybeags <suren0709@gmail.com>
2025-01-17 13:09:51 +0900

    feat: pokemon_data module uploaded

---
+++ chapter12_prettyTable/main.py
@@ -0,0 +1,12 @@
+'''
+외부 패키지의 설치
+
+좌측 상단 메뉴버튼(햄버거) -> file -> settings(설정 : ctrl + alt + s)
+좌측 리스트에서 project : 프로젝트명으로 돼있는 부분 클릭
+-> python interpreter
+-> 좌상단에 + 눌러서 필요한 패키지 검색 및 설치
+'''
+from prettytable import PrettyTable
+
+# PrettyTable 클래스의 객체
+table = PrettyTable()
